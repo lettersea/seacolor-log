6bd0af15ef44b825df31797b8b325a9681644629
lettersea <letter_sea@outlook.com>
2026-02-28 00:21:26 +0800

    test: the correct style cache for chaining invoke

--- test/basic-styles.test.js
+++ test/basic-styles.test.js
@@ -15,6 +15,14 @@ describe("seacolor", () => {
     it("should wrap a text with mutiple styles ANSI according to the order of invoke", () => {
       expect(seacolor.bold.red("text")).toBe(ANSI(1) + ANSI(31) + "text" + ANSI(39) + ANSI(22));
       expect(seacolor.bold.red.underline("text")).toBe(ANSI(1) + ANSI(31) + ANSI(4) + "text" + ANSI(24) + ANSI(39) + ANSI(22));
+      expect(seacolor.blue.bgCyan.strikethrough("text")).toBe(ANSI(34) + ANSI(46) + ANSI(9) + "text" + ANSI(29) + ANSI(49) + ANSI(39));
+    });
+
+    it("should cache the styles for chained styled", () => {
+      const bold = seacolor.bold;
+      expect(bold.styles).toEqual(new Set(["bold"]));
+      expect(bold.red.styles).toEqual(new Set(["bold", "red"]));
+      expect(bold.red.underline.styles).toEqual(new Set(["bold", "red", "underline"]));
     });
   });
 });
